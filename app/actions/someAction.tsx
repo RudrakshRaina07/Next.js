c4045202e24a07a4d84c4bd57787736cc802a383
Rudraksh Raina <1819rudraksh@gmail.com>
2026-09-17 14:00:55 +0530

    server actions

---
+++ app/actions/someAction.tsx
@@ -0,0 +1,8 @@
+"use server"
+
+const someAction = async() => {
+    console.log("Performing some actions");
+    
+}
+
+export default someAction
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Card from "@/components/Card";
 import Image from "next/image";
+import someAction from "./actions/someAction";
 
 export default function Home() {
   return (
       <div>
         <Card title={"Rudraksh"} description={"Hellow from rudraksh"} />
+        <button onClick={someAction}>Click Me</button>
       </div>
   );
 }
